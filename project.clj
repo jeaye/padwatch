f1e82030350ea69bf9eb355712106d6c6ac2db42
jeaye <contact@jeaye.com>
2017-02-04 19:16:55 -0800

    Add basic querying

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [enlive "1.1.6"]]
   :main ^:skip-aot clouse.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clouse/core.clj
+++ src/clouse/core.clj
@@ -1,7 +1,25 @@
 (ns clouse.core
-  (:gen-class))
+  (:gen-class)
+  (:require [net.cgrand.enlive-html :as html]
+            [clojure.pprint :refer :all]))
+
+(def area "sfbay")
+(def section "apa")
+(def base-url (format "https://%s.craigslist.org/search/%s/"
+                      area
+                      section))
+
+(defn fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
+
+(defn query [params]
+  (let [param-strings (map #(str (-> % first name) "=" (second %))
+                           params)
+        joined-params (clojure.string/join "&" param-strings)
+        url (str base-url "?" joined-params)]
+    (fetch-url url)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (pprint (query {:min_price 1500
+                  :max_price 3000})))
