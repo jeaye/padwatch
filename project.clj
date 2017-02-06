2fed3d9df66fa76370ad0d6ce53b4257555f6f8a
jeaye <contact@jeaye.com>
2017-02-05 22:31:49 -0800

    Specify jank license

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-; TODO: Rename to padwatch
 (defproject padwatch "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A continuous apartment listing watcher"
+  :url "https://github.com/jeaye/padwatch"
+  :license {:name "jank license"
+            :url "https://upload.jeaye.com/jank-license"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [enlive "1.1.6"]
