c48ce3da1b7dc9683ba4269caca6eba83e0456dd
jeaye <contact@jeaye.com>
2017-02-11 15:01:16 -0800

    Add final craiglist sleep

--- src/padwatch/source/craigslist.clj
+++ src/padwatch/source/craigslist.clj
@@ -136,9 +136,12 @@
         html-data (query (:params source-config))
         full-row-count (total-count html-data)
         rows (take (:max-rows source-config) (select-rows html-data))
-        used-row-count (count rows)
+        used-row-count (max (count rows) 1) ; Avoid n/0
         ; Take half as long as the cycle length to pull all rows
         row-sleep-ms (/ (/ cycle-length-ms 2) used-row-count)
         row-infos (->> (mapv (partial row-info row-sleep-ms) rows)
                        (filter some?))]
+    (util/sleep (if (zero? (count row-infos))
+                  cycle-length-ms
+                  (/ cycle-length-ms 2)))
     row-infos))
