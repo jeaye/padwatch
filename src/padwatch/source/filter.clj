3440a12f4729debb9e953b83e8d33ff598a52ba3
jeaye <contact@jeaye.com>
2017-02-24 17:38:20 -0800

    Filter posts based on uniqueness of key params

--- src/padwatch/source/filter.clj
+++ src/padwatch/source/filter.clj
@@ -1,5 +1,6 @@
 (ns padwatch.source.filter
   (:require [padwatch
+             [db :as db]
              [config :as config]]))
 
 (defn within?
@@ -8,11 +9,18 @@
    (or (nil? n)
        (and (>= n min) (<= n max)))))
 
-(defn row-filter [html-data row]
+(defn unique? [row-info]
+  (nil? (db/select {:price (:price row-info)
+                    :sqft (:sqft row-info)
+                    :style (:style row-info)
+                    :walkscore (-> row-info :walkscore :score)})))
+
+(defn row-filter [html-data row-info]
   (when
-    (and (within? (:price row) (:min-price config/data) (:max-price config/data))
-         (within? (:sqft row) (:min-sqft config/data) (:max-sqft config/data))
-         (within? (:bedrooms row) (:bedrooms config/data))
-         (within? (:bathrooms row) (:bathrooms config/data))
-         (within? (get-in row [:walkscore :score]) (:min-walkscore config/data) 100))
-    row))
+    (and (within? (:price row-info) (:min-price config/data) (:max-price config/data))
+         (within? (:sqft row-info) (:min-sqft config/data) (:max-sqft config/data))
+         (within? (:bedrooms row-info) (:bedrooms config/data))
+         (within? (:bathrooms row-info) (:bathrooms config/data))
+         (within? (get-in row-info [:walkscore :score]) (:min-walkscore config/data) 100)
+         (unique? row-info))
+    row-info))
